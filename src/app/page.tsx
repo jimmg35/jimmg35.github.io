aa25bf56a50db6a91a475fbc042b3f9238569dd0
jimmg35 <jim60308@gmail.com>
2023-10-16 15:26:43 +0800

    feat: lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
+import React from 'react'
 
 export default function Home() {
   return (
     <main>
-      {/* <button className="inline-block cursor-pointer rounded-md px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-        Button
-      </button> */}
+      <p>About</p>
     </main>
-  );
+  )
 }
