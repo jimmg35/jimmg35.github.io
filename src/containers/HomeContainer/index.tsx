ad859fe0f3366cde36428696c1649faaca2a9db6
jimmg35 <jim60308@gmail.com>
2023-10-21 16:36:27 +0800

    feat: update

--- src/containers/HomeContainer/index.tsx
+++ src/containers/HomeContainer/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+'use client'
+
+import React, { useContext } from 'react'
+import classNames from 'classnames'
+import { ThemeContext } from '../../theme/ThemeProvider'
 
 const HomeContainer = () => {
+  const { themeMode } = useContext(ThemeContext)
   return (
-    <main>
-      <p>Home</p>
+    <main className="flex items-center justify-center">
+      <div className="flex">
+        <div>
+          <p className="text-5xl font-bold">Hello there, I'm Jim 👏</p>
+          <p className="text-4xl font-medium">A software developer</p>
+        </div>
+        <img src={'/image/tax.svg'} className="landing-img w-[160px]" />
+      </div>
     </main>
   )
 }
