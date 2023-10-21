e0a16351d5a31098b033770a937a383ab4ec978a
jimmg35 <jim60308@gmail.com>
2023-10-22 04:37:51 +0800

    feat: update

--- src/containers/HomeContainer/index.tsx
+++ src/containers/HomeContainer/index.tsx
@@ -7,13 +7,32 @@ import { ThemeContext } from '../../theme/ThemeProvider'
 const HomeContainer = () => {
   const { themeMode } = useContext(ThemeContext)
   return (
-    <main className="flex items-center justify-center">
-      <div className="flex">
-        <div>
-          <p className="text-5xl font-bold">Hello there, I'm Jim 👏</p>
-          <p className="text-4xl font-medium">A software developer</p>
+    <main className="flex flex-col items-center justify-center landing-background">
+      <div
+        className={classNames({
+          'p-16 rounded-xl bg-opacity-80 border border-zinc-600': true,
+          'bg-black': themeMode === 'dark',
+          'bg-white': themeMode !== 'dark'
+        })}
+      >
+        <div className="flex items-center gap-16">
+          <div className="flex flex-col gap-2">
+            <p className="text-5xl font-bold">Hello there, I'm Jim 👏</p>
+            <p className="text-4xl font-medium">A software developer</p>
+          </div>
+          <img
+            src={'/image/tax.svg'}
+            className={classNames({
+              'w-[160px]': true,
+              'landing-img': themeMode === 'dark',
+              'landing-img-dark': themeMode !== 'dark'
+            })}
+          />
         </div>
-        <img src={'/image/tax.svg'} className="landing-img w-[160px]" />
+        <div>
+          <button className="btn btn-primary mr-4">About me</button>
+          <button className="btn  btn-accent">C.V. & Resume</button>
+        </div>
       </div>
     </main>
   )
