8682b86b1bc093f9b3db0006211184727deaadad
jimmg35 <jim60308@gmail.com>
2023-10-23 01:06:13 +0800

    feat: lint

--- src/components/PageRouter/index.tsx
+++ src/components/PageRouter/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import React, { useContext } from 'react'
 import classNames from 'classnames'
 import { LocaleContext } from '../../i18n'
 import { RouteContext } from '../../route/RouteProvider'
